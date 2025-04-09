4cd4f731a8511d8cbbb238e6d5a40cfd1ea7b9e0
dromelvan <dromelvan@aland.net>
2025-04-09 23:16:43 +0300

    Put database backups in .tar files

--- pg-backup/backup.sh
+++ pg-backup/backup.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+# Define the backup file name using current date and time
+BACKUP_NAME="d11_$(date +%F_%H-%M-%S)"
+
+# Get the year and month from the current date
 YEAR=$(date +%Y)
 MONTH=$(date +%m)
 
+# Create a directory structure by year and month
 BACKUP_DIR="/backups/$YEAR/$MONTH"
 mkdir -p "$BACKUP_DIR"
 
-pg_dump -h postgres -U d11dbuser -d d11 -c -f "$BACKUP_DIR/d11-prod-$(date +%F_%H%M%S).sql"
+# Perform the pg_dump and create a temporary SQL dump
+pg_dump -h postgres -U postgres -d d11 -F p > /tmp/$BACKUP_NAME.sql
+
+# Create a tar.gz archive and store it in the year/month subdirectory
+tar -czf "$BACKUP_DIR/$BACKUP_NAME.tar.gz" -C /tmp $BACKUP_NAME.sql
+
+# Clean up the temporary SQL dump file
+rm /tmp/$BACKUP_NAME.sql
